b379edbfe12ae077f030a3dcfd063fa9107ee58f
Vincent Beffara <vbeffara@gmail.com>
2022-01-06 00:24:57 +0100

    Tweaks

--- src/graph_theory/cayley.lean
+++ src/graph_theory/cayley.lean
@@ -14,52 +14,32 @@ namespace simple_graph
 
         variables {G : Type} [group G] (S : genset G) (a : G) {x y z : G}
 
-        instance : has_coe (genset G) (finset G) := ⟨λ S, S.els⟩
         instance : has_mem G (genset G) := ⟨λ a s, a ∈ s.els⟩
 
         def adj (x y : G) := x ≠ y ∧ x⁻¹ * y ∈ S
 
-        @[simp] lemma cancel {x y a : G} : (a * x)⁻¹ * (a * y) = x⁻¹ * y
-            := by { simp [mul_assoc] }
+        @[simp] lemma cancel : (a * x)⁻¹ * (a * y) = x⁻¹ * y := by group
 
-        lemma inv_prod {x y : G} : (x⁻¹ * y)⁻¹ = y⁻¹ * x
-            := by { simp }
+        lemma shift_adj : adj S x y -> adj S (a*x) (a*y)
+            := by { rintro ⟨h1,h2⟩, refine ⟨(mul_ne_mul_right a).mpr h1,_⟩, rw cancel, exact h2 }
 
-        lemma shift_adj {a x y : G} : adj S x y -> adj S (a*x) (a*y)
-        := begin
-            rintro ⟨h1,h2⟩, split,
-            { exact (mul_ne_mul_right a).mpr h1 },
-            { rw cancel, assumption }
-        end
+        @[symm] lemma adj_symm (x y) : adj S x y -> adj S y x
+            := by { rintro ⟨h1,h2⟩, refine ⟨h1.symm,_⟩, convert S.sym h2, group }
 
-        @[symm] lemma adj_symm {x y} : adj S x y -> adj S y x
-        := begin
-            rintro ⟨h1,h2⟩, refine ⟨h1.symm,_⟩,
-            rw <-inv_prod, exact S.sym h2
-        end
+        def Cay (S : genset G) : simple_graph G := ⟨adj S, adj_symm S, (λ x, not_and.mpr (λ h1 h2, h1 rfl ))⟩
 
-        def Cay (S : genset G) : simple_graph G := ⟨
-            adj S,
-            by { apply adj_symm },
-            by { intro x, unfold adj, tauto }
-        ⟩
-
-        def shift_path : Π {y : G}, path (Cay S) x y -> path (Cay S) (a*x : G) (a*y : G)
+        @[simp] def shift_path : Π {y : G}, path (Cay S) x y -> path (Cay S) (a*x : G) (a*y : G)
             | _ point   := point
-            | _ (p · h) := shift_path p · shift_adj S h
-
-        @[simp] lemma shift_path_step (p : path (Cay S) x y) (h : (Cay S).adj y z)
-                : shift_path S a (p.step h) = (shift_path S a p).step (shift_adj S h)
-            := rfl
+            | _ (p · h) := shift_path p · shift_adj S a h
 
         @[simp] lemma size_shift_path (p : path (Cay S) x y) : (shift_path S a p).size = p.size
-            := by { induction p, refl, simp [*] }
+            := by { induction p, refl, simpa }
 
         lemma shift : linked (Cay S) x y -> linked (Cay S) (a*x : G) (a*y : G)
             := by { intro h, cases h with p, use shift_path S _ p }
 
         lemma inv : linked (Cay S) (1:G) x -> linked (Cay S) (1:G) (x⁻¹:G)
-            := by { intro h, symmetry, convert shift S x⁻¹ h; simp }
+            := by { intro h, symmetry, convert shift S x⁻¹ h; group }
 
         lemma linked_mp : linked (Cay S) (1:G) x
             := by { apply subgroup.closure_induction,
@@ -68,14 +48,12 @@ namespace simple_graph
                     { intro h, rw <-h at H, exact S.irr H },
                     { convert H, group } },
                 { refl },
-                { intros u v h1 h2, refine linked.trans h1 _, convert shift _ u h2, rw mul_one, },
-                { intros, apply inv, assumption },
+                { intros u v h1 h2, refine linked.trans h1 _, convert shift _ u h2, group },
+                { intros y h, apply inv, exact h },
             }
 
         theorem connected : connected (Cay S)
-            := by { suffices : ∀ x, linked (Cay S) (1:G) x,
-                    { intros x y, transitivity (1:G), apply linked.symm, apply this, apply this },
-                intro, apply linked_mp }
+            := by { intros x y, transitivity (1:G), symmetry, apply linked_mp, apply linked_mp }
 
         instance : connected_graph (Cay S) := ⟨connected S⟩
 
@@ -96,17 +74,15 @@ namespace simple_graph
 
         lemma lipschitz : ∃ K : ℕ, ∀ x y : G, word_dist S2 x y <= K * word_dist S1 x y
             := begin
-                let Δ := finset.image (word_dist S2 1) S1,
+                set Δ := finset.image (word_dist S2 1) S1.els,
                 have : Δ.nonempty := finset.nonempty.image S1.nem (word_dist S2 1),
-                obtain K := finset.max_of_nonempty this,
-                cases K, use K_w,
+                obtain K := finset.max_of_nonempty this, cases K, use K_w,
                 intros x y, obtain p := simple_graph.shortest_path (Cay S1) x y, cases p with p hp,
-                unfold word_dist, rw <-hp, clear hp, induction p with a b h1 h2 ih, simp,
+                unfold word_dist, rw <-hp, clear hp, induction p with a b h1 h2 ih, rw dist_self, exact rfl.ge,
                 transitivity (Cay S2).dist x a + (Cay S2).dist a b, apply simple_graph.dist_triangle,
-                simp, rw [mul_add,mul_one], apply add_le_add ih,
-                have : (Cay S2).dist a b ∈ Δ,
-                    by { simp, use a⁻¹ * b, split, exact h2.2, convert covariant S2 a⁻¹, group },
-                exact finset.le_max_of_mem this K_h
+                dsimp, rw [mul_add,mul_one], apply add_le_add ih,
+                suffices : (Cay S2).dist a b ∈ Δ, by exact finset.le_max_of_mem this K_h,
+                simp, use a⁻¹ * b, split, exact h2.2, convert covariant S2 a⁻¹, group
             end
     end cayley
 end simple_graph
