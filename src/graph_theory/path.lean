ab9952ba56715a8f38b568486e630ce5b1faecee
Vincent Beffara <vbeffara@gmail.com>
2022-01-07 21:52:40 +0100

    Propagate predicates along graph walks

--- src/graph_theory/path.lean
+++ src/graph_theory/path.lean
@@ -75,16 +75,15 @@ namespace simple_graph
         lemma linked_of_subgraph {G₁ G₂ : simple_graph V} (sub : ∀ {x y : V}, G₁.adj x y -> G₂.adj x y) : linked G₁ x y -> linked G₂ x y
             := by { intro h, cases h with p, induction p with a b h1 h2 ih, refl, exact cons (sub ih) p_ih }
 
-        lemma extend {pred : V -> Prop} {hₓ : pred x} {hᵣ : ∀ {a b}, pred a -> G.adj a b -> pred b} : linked G x z -> pred z
-            := by {
-                intro h, cases h.symm with p,
-                suffices : ∀ i ∈ p.support, pred i, { apply this z, exact p.start_mem_support },
-                induction p with a a b c h' p ih; intros i hi,
-                    { simp at hi, rw hi, exact hₓ },
-                    { cases hi,
-                        { subst i, have := ih ⟨p.reverse⟩ b p.start_mem_support, apply hᵣ this h'.symm, exact hₓ },
-                        { apply ih, refine trans h (step h'), exact hi, exact hₓ }
-                    }
-            }
+        def good (G : simple_graph V) (pred : V -> Prop) : Prop := ∀ x y, pred x -> G.adj x y -> pred y
+
+        lemma propagate (pred : V -> Prop) (hg : good G pred) (h : pred x) (p : walk G x y) : pred y
+            := by { induction p with a a b c h₁ p ih, exact h, exact ih (hg a b h h₁) }
+
+        lemma extend (pred : V -> Prop) (hg : good G pred) (h : pred x) : ∀ z, linked G x z -> pred z
+            := by { intros z hz, cases hz with p, exact propagate pred hg h p }
+
+        lemma extend' (conn : connected G) (pred : V -> Prop) (hg : good G pred) (h : pred x) : ∀ z, pred z
+            := λ z, extend pred hg h z (conn x z)
     end linked
 end simple_graph
