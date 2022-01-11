2197d30b1d76a3c0370dba11b8ee6aaf9fdd7822
Vincent Beffara <vbeffara@gmail.com>
2022-01-11 22:54:36 +0100

    Start proof of minor reflexivity

--- src/graph_theory/minor.lean
+++ src/graph_theory/minor.lean
@@ -7,50 +7,53 @@ namespace simple_graph
     open walk
     variables {V V' : Type} {G : simple_graph V}
 
-    namespace contraction
+    namespace contract
         structure setup (G : simple_graph V) :=
             (g : simple_graph V)
             (sub : ∀ {x y : V}, g.adj x y -> G.adj x y)
 
-        def empty_setup : setup G := {
-            g := ⊥,
-            sub := by { intros x y, simp }
-        }
+        namespace setup
+            def support (S : setup G) : Type := V
 
-        def support (S : setup G) : Type := V
+            instance setoid (S : setup G) : setoid S.support
+                := ⟨S.g.linked,simple_graph.linked.equiv⟩
 
-        instance contraction_setoid (S : setup G) : setoid (support S) := ⟨S.g.linked,simple_graph.linked.equiv⟩
+            def clusters (S : setup G) := quotient S.setoid
 
-        def clusters (S : setup G) := quotient (contraction.contraction_setoid S)
+            def adj (S : setup G) (x y : S.clusters) : Prop
+                := x ≠ y ∧ ∃ x' y' : V, ⟦x'⟧ = x ∧ ⟦y'⟧ = y ∧ G.adj x' y'
 
-        variables {S : setup G} {x y : support S}
+            @[symm] lemma symm (S : setup G) (x y : S.clusters) : S.adj x y -> S.adj y x
+                | ⟨h0,x',y',h1,h2,h3⟩ := ⟨h0.symm,y',x',h2,h1,h3.symm⟩
+        end setup
+    end contract
 
-        def adj (x y : clusters S) := x ≠ y ∧ ∃ x' y' : V, ⟦x'⟧ = x ∧ ⟦y'⟧ = y ∧ G.adj x' y'
+    def contract (G : simple_graph V) (S : contract.setup G) : simple_graph S.clusters := ⟨S.adj, S.symm⟩
 
-        @[symm] lemma symm (x y : clusters S) : adj x y -> adj y x
-            | ⟨h0,x',y',h1,h2,h3⟩ := ⟨h0.symm,y',x',h2,h1,h3.symm⟩
+    notation G `/` S := contract G S
 
-        def contract (S : setup G) : simple_graph (clusters S) := ⟨adj, symm⟩
+    namespace contract
+        variables {S : setup G} {x y : S.support} {xx yy : S.clusters}
 
-        lemma proj_adj (h : G.adj x y) : ⟦x⟧ = ⟦y⟧ ∨ (contract S).adj ⟦x⟧ ⟦y⟧
+        lemma proj_adj (h : G.adj x y) : ⟦x⟧ = ⟦y⟧ ∨ (G/S).adj ⟦x⟧ ⟦y⟧
             := dite (⟦x⟧ = ⟦y⟧) or.inl (λ h', or.inr ⟨h',x,y,rfl,rfl,h⟩)
 
-        lemma linked_of_adj : (contract S).adj ⟦x⟧ ⟦y⟧ -> linked G x y
+        lemma linked_of_adj : (G/S).adj ⟦x⟧ ⟦y⟧ -> linked G x y
             | ⟨h₁,a,b,h₂,h₃,h₄⟩ := by { transitivity b, transitivity a,
                 exact linked.linked_of_subgraph S.sub (quotient.eq.mp h₂.symm),
                 exact linked.step h₄,
                 exact linked.linked_of_subgraph S.sub (quotient.eq.mp h₃) }
 
-        noncomputable def proj_path : Π {x y : V}, walk G x y -> walk (contract S) ⟦x⟧ ⟦y⟧
+        noncomputable def proj_path : Π {x y : V}, walk G x y -> walk (G/S) ⟦x⟧ ⟦y⟧
             | _ _ nil                      := nil
             | _ z (cons (h : G.adj x y) p) := dite (⟦y⟧ = ⟦x⟧) (λ h, by { rw <-h, exact proj_path p })
-                                                               (λ h', walk.cons ⟨ne.symm h',_,_,rfl,rfl,h⟩ (proj_path p))
+                                                              (λ h', walk.cons ⟨ne.symm h',_,_,rfl,rfl,h⟩ (proj_path p))
 
-        lemma project_linked : ∀ {x y}, linked G x y -> linked (contract S) ⟦x⟧ ⟦y⟧
+        lemma project_linked : ∀ {x y}, linked G x y -> linked (G/S) ⟦x⟧ ⟦y⟧
             | _ _ ⟨p⟩ := by { induction p with u u v w h p ih, refl,
                 cases proj_adj h with h' h', rw h', exact ih, exact ih.cons h' }
 
-        lemma lift_linked' {xx yy : clusters S} : linked (contract S) xx yy ->
+        lemma lift_linked' : linked (G/S) xx yy ->
                 ∀ (x y : V) (hx : ⟦x⟧ = xx) (hy : ⟦y⟧ = yy), linked G x y
             := by {
                 intro h, cases h with p, induction p with u u v w h p ih; intros x y hx hy,
@@ -59,24 +62,35 @@ namespace simple_graph
                     transitivity a, exact linked_of_adj h, exact ih a y rfl rfl }
             }
 
-        lemma lift_linked (h : linked (contract S) ⟦x⟧ ⟦y⟧) : linked G x y
+        lemma lift_linked (h : linked (G/S) ⟦x⟧ ⟦y⟧) : linked G x y
             := lift_linked' h _ _ rfl rfl
 
-        lemma contract_connected_iff : connected G <-> connected (contract S)
-            := {
-                mp := λ h xx yy, by {
+        lemma contract_connected_iff : connected G <-> connected (G/S)
+            := { mp := λ h xx yy, by {
                     obtain ⟨x, hx : ⟦x⟧ = xx⟩ := quot.exists_rep xx, subst hx,
                     obtain ⟨y, hy : ⟦y⟧ = yy⟩ := quot.exists_rep yy, subst hy,
                     exact project_linked (h x y) },
-                mpr := λ h x y, lift_linked (h ⟦x⟧ ⟦y⟧)
+                mpr := λ h x y, lift_linked (h ⟦x⟧ ⟦y⟧) }
+
+
+        @[simp] def bot {G : simple_graph V} : setup G := ⟨⊥, λ x y, false.rec _⟩
+        instance : has_bot (setup G) := ⟨bot⟩
+
+        lemma proj_bot_inj {x y : (@bot V G).support} : ⟦x⟧ = ⟦y⟧ -> x = y
+            := by { intro h, cases quotient.eq.mp h with p, cases p, refl, simp at p_h, contradiction }
+
+        lemma proj_bot_inj' {x y : (@bot V G).support} : G.adj x y -> ⟦x⟧ ≠ ⟦y⟧
+            | h1 h2 := G.loopless y $ by { rw proj_bot_inj h2 at h1, exact h1 }
+
+        def proj_bot : G →g G/⊥
+            := {
+                to_fun := λ x, ⟦x⟧,
+                map_rel' := λ x y h, ⟨proj_bot_inj' h, ⟨x,y,rfl,rfl,h⟩ ⟩
             }
-    end contraction
+    end contract
 
     def is_minor (G : simple_graph V) (G' : simple_graph V') : Prop
-        := ∃ S : contraction.setup G', embeds_into G (contraction.contract S)
-
-    def is_minor' (G : simple_graph V) (G' : simple_graph V') : Prop
-        := ∃ S : contraction.setup G', ∃ f : G →g contraction.contract S, injective f
+        := ∃ S : contract.setup G', ∃ f : G →g G'/S, injective f
 
     def is_forbidden (H : simple_graph V) (G : simple_graph V') := ¬ (is_minor H G)
 
