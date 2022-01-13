c3b630a81a85bc15ef219a11ffc568ec5b91b4a2
Vincent Beffara <vbeffara@gmail.com>
2022-01-13 01:33:36 +0100

    Clean up

--- src/Scratch.lean
+++ src/Scratch.lean
@@ -1,43 +1,19 @@
-import init.function data.quot tactic
-open function classical quotient
+import tactic
 
-lemma toto : ∀ (V : Type) (s : setoid V) (s' : setoid (quotient s)),
-        ∃ (s'' : setoid V) (φ : quotient s'' -> quotient s'), bijective φ
+variables {V : Type*} (s : setoid V) (t : setoid (quotient s))
 
-    := by { intros, cases s with r hᵣ, cases s' with r' hᵣ',
+def setoid.comp (s : setoid V) (t : setoid (quotient s)) : setoid V :=
+    let f : V → quotient s := quotient.mk,
+        g : quotient s → quotient t := quotient.mk
+    in setoid.ker (g ∘ f)
 
-        let r'' := λ x y, r' (quot.mk r x) (quot.mk r y),
+noncomputable def setoid.comp.iso : quotient (setoid.comp s t) ≃ quotient t :=
+    let f : V → quotient s := quotient.mk,
+        g : quotient s → quotient t := quotient.mk
+    in setoid.quotient_ker_equiv_of_surjective (g ∘ f)
+        ((surjective_quotient_mk (quotient s)).comp (surjective_quotient_mk V))
 
-        have eqv : equivalence r''
-            := ⟨λ _, hᵣ'.1 _, λ _ _ h, hᵣ'.2.1 h, λ x y z h1 h2, hᵣ'.2.2 h1 h2⟩,
-
-        have eqv_r' : eqv_gen r' = r' := equivalence.eqv_gen_eq hᵣ',
-        have eqv_r'' : eqv_gen r'' = r'' := equivalence.eqv_gen_eq eqv,
-
-        let φ := λ xx, quot.mk r' (quot.mk r (quot.out xx)),
-        let ψ := λ xx, quot.mk r'' (quot.out (quot.out xx)),
-
-        have left_inv : left_inverse ψ φ, by {
-            intro x, simp only [φ,ψ],
-            refine eq.trans _ (quot.out_eq x),
-            apply quot.eq.mpr,
-            rw eqv_r'', simp only [r''], rw <-eqv_r',
-            apply quot.eq.mp,
-            rw [eqv_r',quot.out_eq,quot.out_eq] },
-
-        have right_inv : right_inverse ψ φ, by {
-            intro x, simp only [φ,ψ],
-            have h0: ∀ {x y}, r'' x y = r' (quot.mk r x) (quot.mk r y) := λ x y, rfl,
-            have h1 := quot.out_eq x,
-            have h2 := quot.out_eq x.out,
-            rw <-h1, rw <-h2,
-            apply quot.eq.mpr,
-            rw eqv_r', rw <-h0, rw <-eqv_r'',
-            apply quot.eq.mp,
-            rw [eqv_r'',quot.out_eq,quot.out_eq,quot.out_eq] },
-
-        have : bijective φ
-            := ⟨left_inverse.injective left_inv, right_inverse.surjective right_inv⟩,
-
-        exact ⟨⟨r'',eqv⟩,φ,this⟩
-    }
+noncomputable def setoid.comp.iso' : quotient (setoid.comp s t) ≃ quotient t :=
+    let gof : V -> quotient t := quotient.mk ∘ quotient.mk
+    in setoid.quotient_ker_equiv_of_surjective gof
+        ((surjective_quotient_mk (quotient s)).comp (surjective_quotient_mk V))
