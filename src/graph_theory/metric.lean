db058c2614b5ba4f2bed7322991eaf68c5982e6a
Vincent Beffara <vbeffara@gmail.com>
2022-01-24 16:38:10 +0100

    Tweaks

--- src/graph_theory/metric.lean
+++ src/graph_theory/metric.lean
@@ -5,28 +5,28 @@ set_option trace.check true
 namespace simple_graph
     open walk
 
-    variables {V : Type} (G : simple_graph V) [connected_graph G] (a : V) {x y z : V}
+    variables {V : Type} {G : simple_graph V} [connected_graph G] (a : V) {x y z : V}
 
-    def dists (x y) := set.range (length : walk G x y -> ℕ)
+    def dists (G : simple_graph V) (x y : V) := set.range (length : walk G x y -> ℕ)
 
     lemma dists_ne_empty : (dists G x y).nonempty
-        := by { apply set.range_nonempty_iff_nonempty.mpr, apply linked.linked_iff.mp, exact connected_graph.conn x y }
+        := set.range_nonempty_iff_nonempty.mpr (linked.linked_iff.mp (connected_graph.conn x y))
 
-    noncomputable def dist (x y : V)
-        := well_founded.min nat.lt_wf (dists G x y) (dists_ne_empty _)
+    noncomputable def dist (G : simple_graph V) [connected_graph G] (x y : V)
+        := well_founded.min nat.lt_wf (dists G x y) dists_ne_empty
 
     lemma upper_bound (p : walk G x y) : dist G x y <= length p
         := not_lt.mp $ well_founded.not_lt_min _ _ _ (set.mem_range_self p)
 
-    lemma shortest_path (x y) : ∃ p : walk G x y, length p = dist G x y
-        := well_founded.min_mem _ _ (dists_ne_empty _)
+    lemma shortest_path (G : simple_graph V) [connected_graph G] (x y) : ∃ p : walk G x y, length p = dist G x y
+        := well_founded.min_mem _ _ dists_ne_empty
 
     @[simp] lemma dist_self : dist G x x = 0
-        := le_antisymm (upper_bound G (nil : walk G x x)) (zero_le _)
+        := le_antisymm (upper_bound (nil : walk G x x)) (zero_le _)
 
     lemma dist_triangle : dist G x z ≤ dist G x y + dist G y z
         := by { obtain ⟨p,hp⟩ := shortest_path G x y, obtain ⟨q,hq⟩ := shortest_path G y z,
-            rw [<-hp,<-hq,<-length_append], exact upper_bound _ _ }
+            rw [<-hp,<-hq,<-length_append], exact upper_bound _ }
 
     lemma eq_of_dist_eq_zero (h : dist G x y = 0) : x = y
         := by { obtain ⟨p,hₚ⟩ := shortest_path G x y, cases p, refl, rw h at hₚ, contradiction }
@@ -35,14 +35,14 @@ namespace simple_graph
         := by { obtain ⟨p,hₚ⟩ := shortest_path G y x, rw [<-hₚ, <-length_reverse p], apply upper_bound }
 
     lemma dist_comm : dist G x y = dist G y x
-        := le_antisymm (dist_comm' G) (dist_comm' G)
+        := le_antisymm dist_comm' dist_comm'
+
+    noncomputable instance has_dist {V : Type} (G : simple_graph V) [connected_graph G] : has_dist V
+        := ⟨λ x y, simple_graph.dist G x y⟩
+
+    noncomputable instance metric_space {V : Type} (G : simple_graph V) [connected_graph G] : metric_space V
+        :={ dist_self          := λ x, by { unfold has_dist.dist, norm_cast, exact dist_self },
+            eq_of_dist_eq_zero := λ x y, by { unfold has_dist.dist, norm_cast, exact eq_of_dist_eq_zero },
+            dist_comm          := λ x y, by { unfold has_dist.dist, norm_cast, exact dist_comm },
+            dist_triangle      := λ x y z, by { unfold has_dist.dist, norm_cast, exact dist_triangle } }
 end simple_graph
-
-noncomputable instance simple_graph.has_dist {V : Type} (G : simple_graph V) [simple_graph.connected_graph G] : has_dist V
-    := ⟨λ x y, simple_graph.dist G x y⟩
-
-noncomputable instance simple_graph.metric_space {V : Type} (G : simple_graph V) [simple_graph.connected_graph G] : metric_space V
-    :={ dist_self          := by { unfold dist, norm_cast, apply simple_graph.dist_self },
-        eq_of_dist_eq_zero := by { unfold dist, norm_cast, apply simple_graph.eq_of_dist_eq_zero },
-        dist_comm          := by { unfold dist, norm_cast, apply simple_graph.dist_comm },
-        dist_triangle      := by { unfold dist, norm_cast, apply simple_graph.dist_triangle } }
