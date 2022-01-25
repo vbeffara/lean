88db3ab46a07afb54e3874cb1f323bbc1c5c2d6c
Vincent Beffara <vbeffara@gmail.com>
2022-01-25 01:49:38 +0100

    Refactor quotient.lean

--- src/graph_theory/quotient.lean
+++ src/graph_theory/quotient.lean
@@ -2,17 +2,15 @@ import graph_theory.basic graph_theory.path
 import combinatorics.simple_graph.connectivity
 
 namespace setoid
-    variables {V : Type}
-
-    def rel_gen (adj : V -> V -> Prop) (S : setoid V)
-        := eqv_gen.setoid (λ x y, adj x y ∧ S.rel x y)
+    lemma r.symm {V : Type} {S : setoid V} : symmetric S.rel :=
+    λ x y, setoid.symm
 end setoid
 
 namespace simple_graph
     variables {V V' : Type} {G G' : simple_graph V} {S : setoid V}
 
-    def quotient_graph (G : simple_graph V) (S : setoid V) : simple_graph (quotient S)
-    := {
+    def quotient_graph (G : simple_graph V) (S : setoid V) : simple_graph (quotient S) :=
+    {
         adj := λ x y, x ≠ y ∧ ∃ x' y' : V, ⟦x'⟧ = x ∧ ⟦y'⟧ = y ∧ G.adj x' y',
         symm := λ x y ⟨h₀,x',y',h₁,h₂,h₃⟩, ⟨h₀.symm,y',x',h₂,h₁,h₃.symm⟩,
         loopless := λ x ⟨h₀,h⟩, h₀ rfl
@@ -20,38 +18,30 @@ namespace simple_graph
 
     notation G `/` S := quotient_graph G S
 
-    def induced_subgraph (G : simple_graph V) (S : setoid V) : simple_graph V
-    := {
-        adj := λ x y, G.adj x y ∧ S.rel x y,
-        symm := λ x y ⟨h₁,h₂⟩, ⟨h₁.symm,setoid.symm h₂⟩,
-        loopless := λ x ⟨h₁,h₂⟩, G.ne_of_adj h₁ rfl,
-    }
+    namespace quotient_graph
+        def induced_subgraph (G : simple_graph V) (S : setoid V) : simple_graph V :=
+        {
+            adj := G.adj ⊓ S.rel,
+            symm := λ x y ⟨h₁,h₂⟩, ⟨h₁.symm,h₂.symm⟩,
+            loopless := λ x ⟨h,h'⟩, G.ne_of_adj h rfl,
+        }
 
-    lemma induced_le : induced_subgraph G S ≤ G
-    := λ x y h, h.1
+        lemma induced_le : induced_subgraph G S ≤ G :=
+        λ x y h, h.1
 
-    def adapted (S : setoid V) (G : simple_graph V) : Prop
-    := setoid.rel_gen G.adj S = S
+        def adapted (S : setoid V) (G : simple_graph V) : Prop :=
+        relation.refl_trans_gen (G.adj ⊓ S.rel) = S.rel
 
-    def adapted' (S : setoid V) (G : simple_graph V) : Prop
-    := S.rel = (induced_subgraph G S).linked
-
-    def quotient_bot' : V ≃ quotient (⊥ : setoid V)
-    := {
-        to_fun := λ x, quotient.mk' x,
-        inv_fun := λ y, quotient.lift_on' y id (λ _ _, id),
-        left_inv := λ x, by refl,
-        right_inv := λ y, by { induction y; refl },
-    }
-
-    lemma quotient_bot_eq {x y : V} : @quotient.mk' _ ⊥ x = @quotient.mk' _ ⊥ y -> x = y
-    := quotient.eq'.mp
-
-    def quotient_bot : G ≃g G/⊥
-    := {
-        to_equiv := quotient_bot',
-        map_rel_iff' := λ x y, by { split,
-        { rintros ⟨h₁,x',y',h₂,h₃,h₄⟩, rw [quotient_bot_eq h₂, quotient_bot_eq h₃] at h₄, exact h₄ },
-        { intro h, refine ⟨_,x,y,rfl,rfl,h⟩, intro h', rw quotient_bot_eq h' at h, exact G.loopless _ h } }
-    }
+        def iso_bot : G ≃g G/⊥ :=
+        {
+            to_fun := λ x, quotient.mk' x,
+            inv_fun := λ y, quotient.lift_on' y id (λ _ _, id),
+            left_inv := λ x, by refl,
+            right_inv := λ y, by { induction y; refl },
+            map_rel_iff' := λ x y, by { simp only [equiv.coe_fn_mk, quotient_graph], split,
+                { rintros ⟨h₁,x',y',h₂,h₃,h₄⟩, have h₅ : x' = x := quotient.eq'.mp h₂,
+                    have h₆ : y' = y := quotient.eq'.mp h₃, rwa [<-h₅,<-h₆] },
+                { intro h, exact ⟨λ h',G.ne_of_adj h (quotient.eq'.mp h'),x,y,rfl,rfl,h⟩ } }
+        }
+    end quotient_graph
 end simple_graph
