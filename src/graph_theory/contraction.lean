e3e2de027a682441869c99b735c0bac949361b5f
Vincent Beffara <vbeffara@gmail.com>
2022-01-30 21:41:37 +0100

    Separate new version of contraction into separate file

--- src/graph_theory/contraction.lean
+++ src/graph_theory/contraction.lean
@@ -32,154 +32,15 @@ namespace simple_graph
 
         def adapted (S : setoid V) (G : simple_graph V) : Prop :=
         relation.refl_trans_gen (G.adj ⊓ S.rel) = S.rel
-
-        structure setup' (G : simple_graph V) := (S : setoid V) (h : adapted S G)
-
-        namespace setup'
-            open relation
-
-            def of_rel (rel : V -> V -> Prop) (sym : symmetric rel) (sub : rel ≤ G.adj) : setup' G := {
-                S := {
-                    r := relation.refl_trans_gen rel,
-                    iseqv := by { refine ⟨_,_,_⟩,
-                        apply refl_trans_gen.refl,
-                        apply refl_trans_gen.symmetric sym,
-                        apply refl_trans_gen.trans }
-                },
-                h := by { ext a b, split; intro h₁; induction h₁ with u v h₂ h₃ ih,
-                    { refl }, { refine ih.trans _, exact h₃.2 },
-                    { refl }, { refine ih.trans _, refine refl_trans_gen.single _,
-                        split, exact sub _ _ h₃, exact refl_trans_gen.single h₃ } }
-            }
-
-            def bot : setup' G
-            := of_rel (λ x y, false) (λ x y, id) (λ x y, false.rec _)
-        end setup'
     end contraction
 
     def contraction (G : simple_graph V) (S : contraction.setup G) : simple_graph S.clusters
         := G / S.setoid
 
-    def contraction' (G : simple_graph V) (S : contraction.setup' G) : simple_graph (quotient S.S) := G / S.S
-
     notation G `/` S := contraction G S
-    notation G `/` S := contraction' G S
 
     def is_contraction (G : simple_graph V) (G' : simple_graph V') : Prop
         := ∃ S : contraction.setup G', nonempty (G ≃g (G'/S))
-
-    def is_surjective_push (G : simple_graph V) (G' : simple_graph V') : Prop
-        := ∃ φ : V' -> V, surjective φ ∧ G = push φ G'
-
-    example : (∃ S : setoid V', nonempty (G ≃g G'/S)) <-> is_surjective_push G G' :=
-    begin
-        split,
-        { rintro ⟨S,⟨⟨f,f',h₁,h₂⟩,h₃⟩⟩,
-            let φ : V' -> V := f' ∘ quotient.mk', refine ⟨φ,_,_⟩,
-            { exact (left_inverse.right_inverse h₁).surjective.comp quotient.surjective_quotient_mk' },
-            { ext a b, rw <-h₃, simp, split,
-                { rintros ⟨p₁,x,y,p₂,p₃,p₄⟩, refine ⟨ne_of_apply_ne f p₁,x,y,_,_,p₄⟩,
-                    convert congr_arg f' p₂, rw h₁, convert congr_arg f' p₃, rw h₁, },
-                { rintros ⟨p₁,x,y,rfl,rfl,p₂⟩, refine ⟨h₁.injective.ne p₁,x,y,_,_,p₂⟩,
-                    simp [φ], rw h₂, refl, simp [φ], rw h₂, refl } }
-        },
-        { rintro ⟨φ,h₁,h₂⟩, subst G,
-            have K := @setoid.ker_apply_mk_out V' V φ,
-            have L := surj_inv_eq h₁,
-            let S : setoid V' := setoid.ker φ, use S,
-            let ψ := (setoid.quotient_ker_equiv_of_surjective φ h₁).symm, refine ⟨⟨ψ,_⟩⟩,
-            introv, split,
-            { rintro ⟨p₁,x,y,p₂,p₃,p₄⟩, refine ⟨ne_of_apply_ne _ p₁,x,y,_,_,p₄⟩,
-                simp [ψ,setoid.quotient_ker_equiv_of_surjective] at p₂,
-                rw [<-K x,p₂], convert K (surj_inv h₁ a), exact (surj_inv_eq h₁ a).symm,
-                simp [ψ,setoid.quotient_ker_equiv_of_surjective] at p₃,
-                rw [<-K y,p₃], convert K (surj_inv h₁ b), exact (surj_inv_eq h₁ b).symm },
-            { rintro ⟨p₁,x,y,rfl,rfl,p₂⟩, refine ⟨ψ.left_inv.injective.ne p₁,x,y,_,_,p₂⟩,
-                simp [ψ,setoid.quotient_ker_equiv_of_surjective], apply quotient.eq.mpr,
-                apply setoid.ker_def.mpr, rw L,
-                simp [ψ,setoid.quotient_ker_equiv_of_surjective], apply quotient.eq.mpr,
-                apply setoid.ker_def.mpr, rw L }
-        }
-    end
-
-    def adapted (f : V → V') (G : simple_graph V) : Prop :=
-        ∀ (x y : V), f x = f y → ∃ p : walk G x y, ∀ z ∈ p.support, f z = f y
-
-    def adapted' (f : V → V') (G : simple_graph V) : Prop :=
-        ∀ (z : V'), connected (select (λ x, f x = z) G)
-
-    lemma adapted_iff {f : V → V'} : adapted f G ↔ adapted' f G :=
-    begin
-        split,
-        { rintros h₁ z ⟨x,hx⟩ ⟨y,rfl⟩, specialize h₁ x y hx, cases h₁ with p hp,
-            let := select.push_walk p hp, apply linked.linked_iff.mpr, use this },
-        { rintros h₁ x y h₂, specialize h₁ (f y) ⟨x,h₂⟩ ⟨y,rfl⟩, replace h₁ := linked.linked_iff.mp h₁,
-            cases h₁ with p, let := select.pull_walk p, use this, exact select.pull_walk_spec p }
-    end
-
-    def is_contraction' (G : simple_graph V) (G' : simple_graph V') : Prop
-        := ∃ φ : V' -> V, surjective φ ∧ adapted φ G' ∧ G = push φ G'
-
-    example : is_contraction G G' ↔ is_contraction' G G' :=
-    begin
-        split,
-        { rintro ⟨S,⟨⟨f,f',h₁,h₂⟩,h₃⟩⟩, simp [contraction] at h₃, let φ : V' -> V := f' ∘ quotient.mk', refine ⟨φ,_,_,_⟩,
-            { exact (left_inverse.right_inverse h₁).surjective.comp quotient.surjective_quotient_mk' },
-            { rw adapted_iff, intro z, intros x y, rcases x with ⟨x,hx⟩, rcases y with ⟨y,rfl⟩, simp [φ] at hx,
-                replace hx := h₂.left_inverse.injective hx, replace hx := quotient.eq.mp hx,
-                replace hx := linked.linked_iff.mp hx, cases hx with p,
-                suffices : ∀ z ∈ p.support, φ z = φ y, by {
-                    apply linked.linked_of_subgraph (select.mono S.sub),
-                    apply linked.linked_iff.mpr,
-                    use select.push_walk p this },
-                induction p with a a b c hi p ih,
-                { intros z h, cases h, rw h, cases h },
-                { intros z h, cases h, rwa h, suffices : φ a = φ b, rw this at hx, exact ih hx z h,
-                    simp [φ], apply congr_arg, apply quotient.eq.mpr, apply linked.step, exact hi }
-            },
-            { ext a b, rw <-h₃, split,
-                { rintros ⟨p₁,x,y,p₂,p₃,p₄⟩, refine ⟨ne_of_apply_ne f p₁,x,y,_,_,p₄⟩,
-                    convert congr_arg f' p₂, rw h₁, convert congr_arg f' p₃, rw h₁, },
-                { rintros ⟨p₁,x,y,rfl,rfl,p₂⟩, refine ⟨h₁.injective.ne p₁,x,y,_,_,p₂⟩,
-                    simp [φ], rw h₂, refl, simp [φ], rw h₂, refl } }
-        },
-        { rintro ⟨f,h₁,h₂,h₃⟩, subst h₃,
-            let g : simple_graph V' := {
-                adj := λ x' y', f x' = f y' ∧ G'.adj x' y',
-                symm := λ x' y' ⟨h₁,h₂⟩, ⟨h₁.symm,h₂.symm⟩,
-                loopless := λ x' ⟨h₁,h₂⟩, G'.ne_of_adj h₂ rfl },
-            let S : contraction.setup G' := ⟨g, λ x' y' h, h.2⟩, use S, letI : setoid V' := S.setoid,
-            let preimage : V → V' := λ x, classical.some (h₁ x),
-            have p₂ : ∀ x, f (preimage x) = x := λ x, classical.some_spec (h₁ x),
-            let F : V → S.clusters := λ x, ⟦preimage x⟧,
-            let F' : S.clusters → V := λ x, f x.out,
-            have p₃ : ∀ {x y}, S.g.linked x y ↔ f x = f y := by {
-                intros x y, split,
-                { intro h, induction h with a b h₁ h₂ h₃, refl, exact h₃.trans h₂.1 },
-                { intro h, specialize h₂ x y h, cases h₂ with p hp, apply linked.linked_iff.mpr, refine ⟨_⟩,
-                    induction p with a a b c h₃ p ih, refl,
-                    have h₄ : f b = f c := by { apply hp, right, apply walk.start_mem_support },
-                    refine walk.cons ⟨h.trans h₄.symm,h₃⟩ _, apply ih h₄, intros z hz, apply hp, right, exact hz }
-            },
-            have H₁ : left_inverse F' F := by {
-                intro x, simp [F',F], refine eq.trans _ (p₂ x), apply p₃.mp,
-                change ⟦preimage x⟧.out ≈ (preimage x), apply quotient.eq.mp, simp },
-            have H₂ : right_inverse F' F := by {
-                intro x', simp [F',F], refine eq.trans _ (quotient.out_eq _),
-                apply quotient.eq.mpr, apply p₃.mpr, apply p₂ },
-            refine ⟨⟨⟨F,F',H₁,H₂⟩,_⟩⟩,
-            simp [push,contraction,contraction.setup.setoid,quotient_graph,F], intros a b, split,
-            { rintros ⟨h₁,x,h₂,y,h₃,h₄⟩, refine ⟨_,x,_,y,_,h₄⟩,
-                { intro h, rw h at h₁, contradiction },
-                { rw ←p₂ a, apply p₃.mp, exact quotient.eq.mp h₂ },
-                { rw ←p₂ b, apply p₃.mp, exact quotient.eq.mp h₃ } },
-            { rintros ⟨h₁,x,rfl,y,rfl,h₂⟩, refine ⟨_,x,_,y,_,h₂⟩,
-                { intro h, have := quotient.eq.mp h, have := p₃.mp this, rw [p₂ _,p₂ _] at this, exact h₁ this },
-                { apply quotient.eq.mpr, apply p₃.mpr, rw p₂ _ },
-                { apply quotient.eq.mpr, apply p₃.mpr, rw p₂ _ },
-            }
-        }
-    end
 
     infix ` ≼c `:50 := is_contraction
 
@@ -199,8 +60,6 @@ namespace simple_graph
                     },
                     sub := λ x y, by { rintros ⟨h1,h2⟩, cases h2, exact S.sub h2, exact h2.1 }
                 }
-
-            -- TODO : comp.setoid is setoid.ker (setoid.mk ∘ setoid.mk)
 
             namespace comp
                 lemma linked_mp : (S.comp S').g.linked x y -> S'.g.linked ⟦x⟧ ⟦y⟧
@@ -364,13 +223,6 @@ namespace simple_graph
                     { rintro ⟨h1,x',y',h2,h3,h4⟩, rw [<-proj_bot_inj h2, <-proj_bot_inj h3], exact h4 },
                     { intro h, refine ⟨_,x,y,rfl,rfl,h⟩,
                         intro h1, rw proj_bot_inj h1 at h, exact G.ne_of_adj h rfl } } }
-
-        def proj_bot' : G ≃g G/setup'.bot
-            := by { simp only [contraction'], convert quotient_graph.iso_bot;
-                { ext, split; intro h₁,
-                { induction h₁ with u v h₂ h₃ ih, refl, contradiction },
-                { simp only [setoid.bot_def] at h₁, rw h₁ } }
-            }
     end contraction
 
     namespace is_contraction
