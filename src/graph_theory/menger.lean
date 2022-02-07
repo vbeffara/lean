834e49806d210d4910bc32b9048eb68179e3e7cd
Vincent Beffara <vbeffara@gmail.com>
2022-02-07 14:02:00 +0100

    Almost there in the case n=0

--- src/graph_theory/menger.lean
+++ src/graph_theory/menger.lean
@@ -1,55 +1,13 @@
 import combinatorics.simple_graph.connectivity data.finset data.setoid.basic
 import graph_theory.contraction graph_theory.pushforward
 open finset classical function
+open_locale classical
 
 variables {V : Type} [decidable_eq V] [fintype V] {G : simple_graph V} [decidable_rel G.adj] {A B : finset V}
 
 namespace simple_graph
-    -- section contraction_stuff
-    --     def number_of_edges (G : simple_graph V) [decidable_rel G.adj] : ℕ :=
-    --     finset.sum (univ : finset V) (λ v, G.degree v)
-
-    --     notation `∥ ` G ` ∥` := number_of_edges G
-
-    --     def edge_setoid (e : sym2 V) : setoid V :=
-    --     {
-    --         r := λ x y, x = y ∨ (x ∈ e ∧ y ∈ e),
-    --         iseqv := by { refine ⟨_,_,_⟩,
-    --             { intro x, left, refl },
-    --             { intros x y h, cases h, left, exact h.symm, right, exact h.symm },
-    --             { intros x y z h₁ h₂, cases h₂, rw h₂ at h₁, exact h₁,
-    --                 cases h₁, subst h₁, right, exact h₂, right, exact ⟨h₁.1,h₂.2⟩ } }
-    --     }
-
-    --     def edge_contraction (G : simple_graph V) (e : sym2 V) : simple_graph (quotient (edge_setoid e)) :=
-    --     push (@quotient.mk' _ (edge_setoid e)) G
-
-    --     infix `/` := edge_contraction
-
-    --     noncomputable instance (V : Type) [fintype V] [decidable_eq V] (S : setoid V) : decidable_eq (quotient S) :=
-    --     dec_rel _
-
-    --     instance (V : Type) [fintype V] (S : setoid V) [decidable_eq (quotient S)] : fintype (quotient S) :=
-    --     ⟨finset.image quotient.mk univ, λ x, quotient.induction_on' x $ λ a, finset.mem_image.mpr ⟨a, mem_univ a, rfl⟩⟩
-
-    --     noncomputable instance (V : Type) [fintype V] [decidable_eq V] (G : simple_graph V) (e : G.edge_set) :
-    --     decidable_rel (G/e).adj :=
-    --     dec_rel _
-
-    --     lemma lt_edges_of_contract {e : G.edge_set} : ∥ G/e ∥ < ∥ G ∥ :=
-    --     begin
-    --         rcases e with ⟨⟨x,y⟩,h⟩, set e := (⟨⟦(x,y)⟧,h⟩ : G.edge_set),
-    --         letI : setoid V := edge_setoid e,
-    --         have : (G/e).degree ⟦x⟧ + 1 = G.degree x := by {
-    --             simp [degree,neighbor_finset], sorry,
-    --         },
-    --         sorry
-    --     end
-    -- end contraction_stuff
-
     structure AB_path (G : simple_graph V) (A B : finset V) :=
-        {a b : V} (p : walk G a b)
-        (ha : a ∈ A) (hb : b ∈ B)
+        {a b : V} (p : walk G a b) (ha : a ∈ A) (hb : b ∈ B)
 
     def separates (G : simple_graph V) (A B : finset V) (X : finset V) : Prop :=
     ∀ γ : AB_path G A B, ∃ z : V, z ∈ X ∧ z ∈ γ.p.support
@@ -72,26 +30,99 @@ namespace simple_graph
 
     def joinable (G : simple_graph V) (A B : finset V) : Prop := nonempty (AB_path G A B)
 
-    structure path_set (G : simple_graph V) (A B : finset V) extends finset (AB_path G A B) :=
-    (disjoint : ∀ {γ₁ γ₂ : to_finset} {z : V}, z ∈ γ₁.val.p.support → z ∈ γ₂.val.p.support → γ₁ = γ₂)
+    structure path_set (G : simple_graph V) (A B : finset V) :=
+        (P : finset (AB_path G A B))
+        (disjoint : ∀ {γ₁ γ₂ : P} {z : V}, z ∈ γ₁.val.p.support → z ∈ γ₂.val.p.support → γ₁ = γ₂)
 
-    lemma path_le_cut {P : path_set G A B} {X : cut_set G A B} : P.to_finset.card ≤ X.X.card :=
+    lemma path_le_cut {P : path_set G A B} {X : cut_set G A B} : P.P.card ≤ X.X.card :=
     begin
-        let φ : Π γ : P.to_finset, {z : V // z ∈ X.X ∧ z ∈ γ.val.p.support} :=
+        let φ : Π γ : P.P, {z : V // z ∈ X.X ∧ z ∈ γ.val.p.support} :=
             λ γ, subtype_of_exists (X.sep γ),
-        let ψ : P.to_finset → X.X := λ γ, ⟨φ γ, (φ γ).prop.1⟩,
+        let ψ : P.P → X.X := λ γ, ⟨φ γ, (φ γ).prop.1⟩,
         have h₁ : ∀ γ, (ψ γ).val ∈ γ.val.p.support := λ γ, (φ γ).prop.2,
         have ψ_inj : injective ψ := λ i j h, P.disjoint (h₁ i) (by { rw h, exact h₁ j }),
         have := fintype.card_le_of_injective ψ ψ_inj,
         simp_rw [←fintype.card_coe], convert this,
     end
 
-    lemma upper_bound (P : path_set G A B) : fintype.card P.to_finset ≤ min_cut G A B :=
+    lemma upper_bound (P : path_set G A B) : fintype.card P.P ≤ min_cut G A B :=
     begin
         have h₁ := well_founded.min_mem nat.lt_wf (cut_set_sizes G A B)
             (set.range_nonempty_iff_nonempty.mpr nonempty_cut_set),
         obtain ⟨X,hX⟩ := subtype_of_exists h₁, simp at hX, rw [min_cut,←hX,fintype.card_coe],
         exact path_le_cut
+    end
+
+    lemma bot_iff_no_edge : ∥G∥ = 0 ↔ G = ⊥ :=
+    begin
+        split; intro h,
+        { ext x y, simp, intro h₁, let xy : sym2 V := ⟦(x,y)⟧,
+            have h₂ : xy ∈ G.edge_set := h₁,
+            let e : G.edge_set := ⟨⟦(x,y)⟧,h₂⟩,
+            have := fintype.card_eq_zero_iff.mp h,
+            exact is_empty_iff.mp this e },
+        { simp_rw h, apply fintype.card_eq_zero_iff.mpr,
+            apply is_empty_iff.mpr, rintro ⟨e,h₁⟩,
+            induction e, change false at h₁, contradiction, refl }
+    end
+
+    lemma bot_separates_if {X : finset V} : separates ⊥ A B X ↔ (A ∩ B) ⊆ X :=
+    begin
+        split; intro h,
+        { rintros z hz, simp at hz,
+            let γ : AB_path ⊥ A B := ⟨(walk.nil : walk ⊥ z z), hz.1, hz.2⟩,
+            rcases (h γ) with ⟨x,h₁,h₂⟩, simp at h₂, subst x, exact h₁ },
+        { rintro ⟨a,b,γ,ha,hb⟩, cases γ, swap, change false at γ_h, contradiction,
+            simp, apply mem_of_subset h, simp, exact ⟨ha,hb⟩ }
+    end
+
+    lemma bot_min_cut : min_cut ⊥ A B = (A ∩ B).card :=
+    begin
+        sorry
+    end
+
+    noncomputable def bot_path_set (A B : finset V) : {P : path_set ⊥ A B // P.P.card = (A ∩ B).card} :=
+    begin
+        let φ : A ∩ B → AB_path ⊥ A B :=
+        by { intro z,
+            have h' : z.val ∈ A ∧ z.val ∈ B := mem_inter.mp z.property,
+            exact AB_path.mk walk.nil h'.1 h'.2
+        },
+        let S := image φ univ,
+
+        have φ_inj : injective φ := by {
+            rintros ⟨x,hx⟩ ⟨y,hy⟩ h, simp [φ] at h, cases h, subst y
+        },
+
+        have good_card : S.card = (A ∩ B).card := by {
+            rw card_image_of_injective univ φ_inj,
+            rw finset.card_univ,
+            convert fintype.card_of_finset (A ∩ B) _,
+            intro z, simp, split,
+            { rintros ⟨h₁,h₂⟩, exact set.mem_sep h₁ h₂ },
+            { rintros h₁, exact h₁ }
+        },
+
+        refine ⟨path_set.mk S _, good_card⟩,
+
+        rintro ⟨⟨a₁,b₁,γ₁,h₁,h₂⟩,h₃⟩, cases γ₁, swap, change false at γ₁_h, contradiction,
+        rintro ⟨⟨a₂,b₂,γ₂,h₄,h₅⟩,h₆⟩, cases γ₂, swap, change false at γ₂_h, contradiction,
+        simp only [walk.support_nil, list.mem_singleton, subtype.mk_eq_mk, and_self_left, forall_eq],
+        intro a₁a₂, subst a₁a₂, simp only [eq_self_iff_true, heq_iff_eq, and_self]
+    end
+
+    lemma lower_bound_aux (n : ℕ) : ∀ (G : simple_graph V), ∥G∥ = n → ∃ P : path_set G A B, P.P.card = G.min_cut A B :=
+    begin
+        induction n using nat.strong_induction_on with n ih, simp at ih,
+        by_cases (n=0),
+        { subst n, intros G hG, replace hG := bot_iff_no_edge.mp hG, subst G,
+            let P := bot_path_set A B, use P.val, convert P.prop, exact bot_min_cut },
+        { sorry }
+    end
+
+    lemma lower_bound : ∃ P : path_set G A B, P.P.card = G.min_cut A B :=
+    begin
+        apply lower_bound_aux, exact rfl
     end
 
     -- theorem menger (h : separable G A B) : max_path_number G A B = min_cut h :=
