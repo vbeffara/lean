56ef2c050672a9a417a03bf75d9ee5a619b92f03
Vincent Beffara <vbeffara@gmail.com>
2022-03-25 14:28:53 +0100

    Clean up

--- src/graph_theory/cayley.lean
+++ src/graph_theory/cayley.lean
@@ -12,24 +12,24 @@ structure genset (G : Type*) [group G] :=
   (nem : els.nonempty)
   (irr : (1:G) ∉ els)
 
-variables {G : Type*} [group G] {S : genset G} (S1 S2 : genset G) (a : G) {x y z : G}
+variables {G : Type*} [group G] {S S1 S2 : genset G} (a : G) {x y z : G}
 
 instance : has_mem G (genset G) := ⟨λ a s, a ∈ s.els⟩
 
-def adj (S : genset G) (x y : G) := x⁻¹ * y ∈ S
+def genset.adj (S : genset G) (x y : G) := x⁻¹ * y ∈ S
 
-lemma shift_adj (h : adj S x y) : adj S (a*x) (a*y) :=
-by { unfold adj, convert h using 1, group }
+lemma shift_adj (h : S.adj x y) : S.adj (a*x) (a*y) :=
+by { unfold genset.adj, convert h using 1, group }
 
-@[symm] lemma adj_symm (x y) (h : adj S x y) : adj S y x
-:= by { unfold adj, convert S.sym h, group }
+@[symm] lemma adj_symm (x y) (h : S.adj x y) : S.adj y x
+:= by { unfold genset.adj, convert S.sym h, group }
 
 def Cay (S : genset G) : simple_graph G :=
-{ adj := adj S,
+{ adj := S.adj,
   symm := adj_symm,
   loopless := λ x h, S.irr (by { convert h, group }) }
 
-def left_shift : (Cay S) →g (Cay S) :=
+def left_shift : Cay S →g Cay S :=
 ⟨(*) a, λ x y, shift_adj a⟩
 
 def shift_path (p : walk (Cay S) x y) : walk (Cay S) (a*x) (a*y) :=
@@ -45,13 +45,13 @@ lemma reachable_mp : reachable (Cay S) 1 x :=
 begin
   apply subgroup.closure_induction,
   { rw S.gen, trivial },
-  { intros y h, apply reachable.step, simp only [Cay,adj], convert h, group },
+  { intros y h, apply reachable.step, simp only [Cay,genset.adj], convert h, group },
   { refl },
   { intros u v h1 h2, refine reachable.trans h1 _, convert shift u h2, group },
   { intros y h, apply inv, exact h }
 end
 
-theorem connected (S : genset G) : connected (Cay S) :=
+theorem Cay.connected : connected (Cay S) :=
 begin
   split,
   { intros x y, transitivity (1:G), symmetry, apply reachable_mp, apply reachable_mp },
@@ -64,8 +64,8 @@ set.range (length : walk G x y -> ℕ)
 
 lemma covariant' : (Cay S).dist (a*x) (a*y) ≤ (Cay S).dist x y :=
 begin
-  obtain ⟨p,hp⟩ := (connected S).exists_walk_of_dist x y, rw ←hp, clear hp,
-  rw ←walk.length_map, exact dist_le (shift_path a p)
+  obtain ⟨p,hp⟩ := Cay.connected.exists_walk_of_dist x y,
+  rw [←hp,←walk.length_map], exact dist_le (shift_path a p)
 end
 
 lemma covariant : (Cay S).dist (a*x) (a*y) = (Cay S).dist x y :=
@@ -79,17 +79,19 @@ some_spec (max_of_nonempty (S1.nem.image ((Cay S2).dist 1)))
 
 lemma distorsion_le {h : (Cay S1).adj x y} : (Cay S2).dist x y ≤ distorsion S1 S2 :=
 begin
-  refine finset.le_max_of_mem _ (distorsion_spec S1 S2),
+  refine finset.le_max_of_mem _ distorsion_spec,
   rw [finset.mem_image], refine ⟨x⁻¹ * y, h, _⟩, convert covariant x⁻¹, group
 end
 
 lemma lipschitz : (Cay S2).dist x y <= (distorsion S1 S2) * (Cay S1).dist x y :=
 begin
-  obtain ⟨p,hp⟩ := (connected S1).exists_walk_of_dist x y, rw <-hp, clear hp,
+  have : ∃ (p : (Cay S1).walk x y), p.length = (Cay S1).dist x y :=
+    Cay.connected.exists_walk_of_dist x y,
+  obtain ⟨p,hp⟩ := this, rw <-hp, clear hp,
   induction p with u u v w h p ih,
   { simp only [dist_self, walk.length_nil, mul_zero] },
   { simp only [walk.length_cons], transitivity (Cay S2).dist u v + (Cay S2).dist v w,
-    apply (connected S2).dist_triangle, rw [mul_add,mul_one,add_comm],
+    apply Cay.connected.dist_triangle, rw [mul_add,mul_one,add_comm],
     apply add_le_add ih, apply distorsion_le, exact h }
 end
 
