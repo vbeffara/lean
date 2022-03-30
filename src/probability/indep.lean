c6a8a4684c9293be016304e2a4805576d1a21d26
Vincent Beffara <vbeffara@gmail.com>
2022-03-30 17:31:38 +0200

    Some lemmas

--- src/probability/indep.lean
+++ src/probability/indep.lean
@@ -10,6 +10,51 @@ variables {Ω : Type*} [measure_space Ω] [is_probability_measure (volume : meas
 
 namespace probability_theory
 
+lemma lintegral_mul_indicator_eq_lintegral_mul_lintegral_indicator'
+  {α : Type*} {Mf : measurable_space α} [M : measurable_space α] (μ : measure α) (hMf : Mf ≤ M)
+  (c : ennreal) {T : set α} (h_meas_T : measurable_set T)
+  (h_ind : @indep_sets α M Mf.measurable_set' {T} μ)
+  {f : α → ennreal} (h_meas_f : ae_measurable' Mf f μ) :
+  ∫⁻ a, f a * T.indicator (λ _, c) a ∂μ = ∫⁻ a, f a ∂μ * ∫⁻ a, T.indicator (λ _, c) a ∂μ :=
+begin
+  rcases h_meas_f with ⟨g,g_meas,g_ae⟩,
+  rw lintegral_congr_ae g_ae,
+  convert @lintegral_mul_indicator_eq_lintegral_mul_lintegral_indicator
+    α Mf M μ hMf c T h_meas_T h_ind g g_meas using 1,
+  apply lintegral_congr_ae, apply g_ae.mul, apply ae_of_all, intro, refl
+end
+
+lemma preimage_ae_eq_of_ae_eq {α β : Type*} [measurable_space α] [measurable_space β]
+  {f g : α → β} {μ : measure α} (hfg : f =ᵐ[μ] g) {B : set β} :
+f ⁻¹' B =ᵐ[μ] g ⁻¹' B :=
+hfg.fun_comp B
+
+lemma indep_fun_of_indep_fun_of_ae_eq {α β : Type*} [measurable_space α] [measurable_space β]
+  {f g f' g' : α → β} {μ : measure α} (hfg : indep_fun f g μ) (hf : f =ᵐ[μ] f') (hg : g =ᵐ[μ] g') :
+indep_fun f' g' μ :=
+begin
+  rintro _ _ ⟨A,hA,rfl⟩ ⟨B,hB,rfl⟩,
+  have h1 : f ⁻¹' A =ᵐ[μ] f' ⁻¹' A := hf.fun_comp A,
+  have h2 : g ⁻¹' B =ᵐ[μ] g' ⁻¹' B := hg.fun_comp B,
+  rw [←measure_congr h1, ←measure_congr h2, ←measure_congr (h1.inter h2)],
+  apply hfg, exact ⟨_,hA,rfl⟩, exact ⟨_,hB,rfl⟩
+end
+
+lemma lintegral_mul_eq_lintegral_mul_lintegral_of_indep_fun'
+  {α : Type*} [measurable_space α] {μ : measure α}
+  {f g : α → ennreal} (h_meas_f : ae_measurable f μ) (h_meas_g : ae_measurable g μ)
+  (h_indep_fun : indep_fun f g μ) :
+  ∫⁻ a, (f * g) a ∂μ = ∫⁻ a, f a ∂μ * ∫⁻ a, g a ∂μ :=
+begin
+  rcases h_meas_f with ⟨f',f'_meas,f'_ae⟩,
+  rcases h_meas_g with ⟨g',g'_meas,g'_ae⟩,
+  have := indep_fun_of_indep_fun_of_ae_eq h_indep_fun f'_ae g'_ae,
+  have := lintegral_mul_eq_lintegral_mul_lintegral_of_indep_fun f'_meas g'_meas this,
+  convert this using 1,
+  { apply lintegral_congr_ae, exact f'_ae.mul g'_ae },
+  { rw [lintegral_congr_ae f'_ae, lintegral_congr_ae g'_ae] }
+end
+
 lemma indep_fun_comp_of_indep_fun {α α' β β' : Type*}
   [measurable_space α] [measurable_space α'] [measurable_space β] [measurable_space β']
   {f : Ω → α} {g : Ω → α'} (hfg : indep_fun f g)
@@ -53,9 +98,6 @@ begin
     hXmes.ennreal_of_real hYmes.ennreal_of_real (indep_fun_comp_of_indep_fun hXYind);
   exact ennreal.measurable_of_real
 end
-
-example {X : Ω → ℝ} : measurable X → measurable (X⁺) :=
-λ h, measurable.sup_const h 0
 
 lemma integral_indep {X Y : Ω → ℝ} {h : indep_fun X Y}
   {hXm : measurable X} {hX : integrable X} {hYm : measurable Y} {hY : integrable Y} :
