c6909fb86defbb8ab3b959304fe47a6bf1eb3afe
Vincent Beffara <vbeffara@gmail.com>
2022-04-14 23:17:16 +0200

    Variance is additive

--- src/probability/lln.lean
+++ src/probability/lln.lean
@@ -10,88 +10,47 @@ open measure_theory probability_theory
 open_locale big_operators measure_theory probability_theory
 noncomputable theory
 
+variables {α : Type*} {mα : measurable_space α} {μ : measure α}
 variables {Ω : Type*} [measure_space Ω] [is_probability_measure (volume : measure Ω)]
-variables {X : ℕ → Ω → ℝ} {n : ℕ} {μ : probability_measure ℝ}
 
-def is_iid (μ : probability_measure ℝ) (X : ℕ → Ω → ℝ) : Prop :=
-  Indep_fun (λ _, real.measurable_space) X volume ∧
-  ∀ n, measure.map (X n) volume = μ
+def cov (X Y : Lp ℝ 1 μ) : ℝ := integral μ (X * Y) - integral μ X * integral μ Y
 
-def has_first_moment (μ : probability_measure ℝ) : Prop :=
-@has_finite_integral ℝ ℝ _ (by apply_instance) id μ.val
-
-def recenter (X : Ω → ℝ) (ω : Ω) : ℝ := X ω - 𝔼[X]
-
-lemma recenter_add {X Y : Ω → ℝ} {hX : integrable X} {hY : integrable Y} :
-  recenter (X + Y) = recenter X + recenter Y :=
-begin
-  ext ω, simp only [recenter, pi.add_apply], rw integral_add hX hY, ring
-end
-
-def shift (a : ℝ) (x : ℝ) : ℝ := x + a
-
-lemma recenter_shift {X : Ω → ℝ} : recenter X = shift (- 𝔼[X]) ∘ X := rfl
-
-lemma recenter_comap {X : Ω → ℝ} :
-  measurable_space.comap (recenter X) real.measurable_space =
-  measurable_space.comap X real.measurable_space :=
-begin
-  ext A, simp [measurable_space.comap], split,
-  { rintro ⟨s,hs1,hs2⟩, use (shift (- 𝔼[X])) ⁻¹' s, split,
-    { exact measurable_set_preimage (measurable_add_const _) hs1 },
-    { rwa [←set.preimage_comp,←recenter_shift] } },
-  { rintro ⟨s,hs1,hs2⟩, use (shift 𝔼[X]) ⁻¹' s, split,
-    { exact measurable_set_preimage (measurable_add_const _) hs1 },
-    { rw [←set.preimage_comp,recenter_shift,←hs2], ext, simp [shift] } }
-end
-
-def cov (X Y : Ω → ℝ) : ℝ := 𝔼[X * Y] - 𝔼[X] * 𝔼[Y]
-
-lemma cov_eq_zero_of_indep {X Y : Ω → ℝ} {hX : integrable X} {hY : integrable Y}
-  (h : indep_fun X Y) :
+lemma cov_eq_zero_of_indep {X Y : Lp ℝ 1 μ} (h : indep_fun X Y μ) :
   cov X Y = 0 :=
-by simp_rw [cov, indep_fun.integral_mul_of_integrable h hX hY, sub_self]
+sub_eq_zero_of_eq (h.integral_mul_of_integrable (L1.integrable_coe_fn X) (L1.integrable_coe_fn Y))
 
-noncomputable def var (X : Ω → ℝ) : ℝ := 𝔼[X ^ 2] - 𝔼[X] ^ 2
+def var (X : Lp ℝ 2 μ) : ℝ := integral μ (X^2) - (integral μ X)^2
+
+lemma blah [is_finite_measure μ] (f : Lp ℝ 2 μ) : integrable f μ :=
+begin
+  convert L1.integrable_coe_fn (⟨f, Lp.antitone _ f.prop⟩ : Lp ℝ 1 μ),
+  norm_cast,
+  exact one_le_two
+end
+
+lemma var.add [is_finite_measure μ] {X Y : Lp ℝ 2 μ} (h : indep_fun X Y μ) :
+  var (X + Y) = var X + var Y :=
+begin
+  have h1 : integrable X μ := blah X,
+  have h2 : integrable Y μ := blah Y,
+  have h4 := indep_fun.integral_mul_of_integrable h h1 h2,
+  have h5 := Lp.coe_fn_add X Y,
+  have h6 := h5.mul h5, simp_rw [← pi.mul_apply] at h6,
+  have h7 : indep_fun Y X μ := h.symm,
+  have h8 := h7.integrable_mul h2 h1,
+  have ha := @L2.integrable_inner α ℝ ℝ _ mα μ _ X X,
+  have hb := @L2.integrable_inner α ℝ ℝ _ mα μ _ X Y,
+  have hc := @L2.integrable_inner α ℝ ℝ _ mα μ _ Y X,
+  have hd := @L2.integrable_inner α ℝ ℝ _ mα μ _ Y Y,
+
+  simp_rw [var, pow_two, integral_congr_ae h5, integral_congr_ae h6, add_mul, mul_add],
+  repeat { rw [integral_add'] },
+  assumption',
+  { rw [h.integral_mul_of_integrable h1 h2, h7.integral_mul_of_integrable h2 h1],
+    ring },
+  { exact ha.add hb },
+  { exact hc.add hd },
+end
 
 noncomputable def partial_avg (X : ℕ → Ω → ℝ) (n : ℕ) (ω : Ω) : ℝ :=
 (∑ i in finset.range n, X i ω) / n
-
-lemma blah {f : Ω → ℝ} (hf : measurable f) : integrable (f*f) → integrable f := by
-{
-  rintro ⟨h1,h2⟩,
-  refine ⟨hf.ae_strongly_measurable, _⟩,
-  simp [has_finite_integral],
-  rw [←measure_theory.snorm_one_eq_lintegral_nnnorm],
-  apply lt_of_le_of_lt
-    (@snorm_le_snorm_mul_rpow_measure_univ Ω ℝ _ volume _ 1 2 _ f hf.ae_strongly_measurable),
-  rw snorm_eq_lintegral_rpow_nnnorm,
-  simp [measure_univ],
-  apply ennreal.rpow_lt_top_of_nonneg,
-  norm_num,
-  apply ne_top_of_lt,
-  have : (2 : ℝ) = ↑(2 : ℕ) := by norm_num,
-  simp_rw [this, ennreal.rpow_nat_cast, pow_two],
-  convert h2, funext, simp,
-  norm_num,
-  norm_num,
-  apply ennreal.coe_le_coe.mpr, simp
-}
-
-lemma var_sum {X Y : Ω → ℝ} {hXm : measurable X} {hYm : measurable Y}
-  (h : indep_fun X Y) (hX : integrable (X*X)) (hY : integrable (Y*Y)) :
-  var (X + Y) = var X + var Y :=
-begin
-  have h1 : integrable X := blah hXm hX,
-  have h2 : integrable Y := blah hYm hY,
-  have h3 : integrable (X*Y) := indep_fun.integrable_mul h h1 h2,
-  have hh := indep_fun.integral_mul_of_integrable h h1 h2,
-
-  apply eq_of_sub_eq_zero,
-  simp_rw [var, pow_two, mul_add, add_mul, @mul_comm _ _ Y X],
-  repeat { rw [integral_add'] },
-  { ring_nf, rw [hh], ring },
-  assumption',
-  { exact hX.add h3 },
-  { exact h3.add hY },
-end
