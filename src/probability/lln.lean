f8d3ca0191ca5f2222433ee10fc36532ef5216f2
Vincent Beffara <vbeffara@gmail.com>
2022-04-15 09:17:39 +0200

    Golf

--- src/probability/lln.lean
+++ src/probability/lln.lean
@@ -7,40 +7,36 @@ variables {Ω : Type*} [measure_space Ω] [is_probability_measure (volume : meas
 
 noncomputable def cov (X Y : Lp ℝ 1 μ) : ℝ := integral μ (X * Y) - integral μ X * integral μ Y
 
-lemma cov_eq_zero_of_indep {X Y : Lp ℝ 1 μ} (h : indep_fun X Y μ) :
+lemma indep_fun.cov_eq_zero {X Y : Lp ℝ 1 μ} (h : indep_fun X Y μ) :
   cov X Y = 0 :=
 sub_eq_zero_of_eq (h.integral_mul_of_integrable (L1.integrable_coe_fn X) (L1.integrable_coe_fn Y))
 
 noncomputable def var (X : Lp ℝ 2 μ) : ℝ := integral μ (X^2) - (integral μ X)^2
 
-lemma blah [is_finite_measure μ] (f : Lp ℝ 2 μ) : integrable f μ :=
-begin
-  have : (1 : ennreal) ≤ 2 := by { norm_cast, exact one_le_two },
-  convert L1.integrable_coe_fn (⟨f, Lp.antitone this f.prop⟩ : Lp ℝ 1 μ)
-end
+lemma Lp.integrable [is_finite_measure μ] {p : ennreal} (hp : 1 ≤ p) (f : Lp ℝ p μ) :
+  integrable f μ :=
+by convert L1.integrable_coe_fn (⟨f, Lp.antitone hp f.prop⟩ : Lp ℝ 1 μ)
 
 lemma var.add [is_finite_measure μ] {X Y : Lp ℝ 2 μ} (h : indep_fun X Y μ) :
   var (X + Y) = var X + var Y :=
 begin
-  have h1 : integrable X μ := blah X,
-  have h2 : integrable Y μ := blah Y,
-  have h4 := indep_fun.integral_mul_of_integrable h h1 h2,
+  have l2 : (1 : ennreal) ≤ 2 := by { norm_cast, exact one_le_two },
+  have hh : ∀ f g : Lp ℝ 2 μ, integrable (f * g) μ := @L2.integrable_inner α ℝ ℝ _ mα μ _,
+  have h' : indep_fun Y X μ := h.symm,
+  have hX : integrable X μ := Lp.integrable l2 X,
+  have hY : integrable Y μ := Lp.integrable l2 Y,
+  have h1 : integrable (X * X) μ := hh X X,
+  have h2 : integrable (X * Y) μ := hh X Y,
+  have h3 : integrable (Y * X) μ := hh Y X,
+  have h4 : integrable (Y * Y) μ := hh Y Y,
   have h5 := Lp.coe_fn_add X Y,
-  have h6 := h5.mul h5, simp_rw [← pi.mul_apply] at h6,
-  have h7 : indep_fun Y X μ := h.symm,
-  have h8 := h7.integrable_mul h2 h1,
-  have ha := @L2.integrable_inner α ℝ ℝ _ mα μ _ X X,
-  have hb := @L2.integrable_inner α ℝ ℝ _ mα μ _ X Y,
-  have hc := @L2.integrable_inner α ℝ ℝ _ mα μ _ Y X,
-  have hd := @L2.integrable_inner α ℝ ℝ _ mα μ _ Y Y,
+  have h6 := h5.mul h5,
+  simp_rw [← pi.mul_apply] at h6,
 
   simp_rw [var, pow_two, integral_congr_ae h5, integral_congr_ae h6, add_mul, mul_add],
-  repeat { rw [integral_add'] },
-  assumption',
-  { rw [h.integral_mul_of_integrable h1 h2, h7.integral_mul_of_integrable h2 h1],
-    ring },
-  { exact ha.add hb },
-  { exact hc.add hd },
+  rw [integral_add' (h1.add h2) (h3.add h4), integral_add' h1 h2, integral_add' h3 h4,
+    integral_add' hX hY, h.integral_mul_of_integrable hX hY, h'.integral_mul_of_integrable hY hX],
+  ring
 end
 
 noncomputable def partial_avg (X : ℕ → Ω → ℝ) (n : ℕ) : Ω → ℝ :=
